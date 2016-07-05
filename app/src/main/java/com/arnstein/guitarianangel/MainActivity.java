be736bbe0a8653b639d3ecedf8c3d1cecce9881c
Joey <joeyarnstein@gmail.com>
2016-07-05 15:33:37 -0700

    refactor main activity to implement View on click listener inface

--- app/src/main/java/com/arnstein/guitarianangel/MainActivity.java
+++ app/src/main/java/com/arnstein/guitarianangel/MainActivity.java
@@ -10,11 +10,15 @@ import android.widget.EditText;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.searchTabButton) Button mSearchTabButton;
-    @Bind(R.id.searchTablatureTextEdit) EditText mSearchTablature;
-    @Bind(R.id.searchLessonButton) Button mSearchLessonButton;
-    @Bind(R.id.searchLessonsTextEdit) EditText mSearchLessons;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.searchTabButton)
+    Button mSearchTabButton;
+    @Bind(R.id.searchTablatureTextEdit)
+    EditText mSearchTablature;
+    @Bind(R.id.searchLessonButton)
+    Button mSearchLessonButton;
+    @Bind(R.id.searchLessonsTextEdit)
+    EditText mSearchLessons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,26 +26,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mSearchTabButton.setOnClickListener(new View.OnClickListener() { //add click listener
+        mSearchTabButton.setOnClickListener(this);
+        mSearchLessonButton.setOnClickListener(this);
 
-            @Override
-            public void onClick(View v) {
-                String searchTablature = mSearchTablature.getText().toString();
-                Intent intent = new Intent(MainActivity.this, BrowseMuisc.class);
-                intent.putExtra("tablatureSearch", searchTablature);
-                startActivity(intent);
-            }
-        });
+    }
 
-        mSearchLessonButton.setOnClickListener(new View.OnClickListener() { //add click listener
-
-            @Override
-            public void onClick(View v) {
-                String searchLesson = mSearchLessons.getText().toString();
-                Intent intent2 = new Intent(MainActivity.this, BrowseLessons.class);
-                intent2.putExtra("lessonSearch", searchLesson);
-                startActivity(intent2);
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        if (v == mSearchTabButton) {
+            String searchTablature = mSearchTablature.getText().toString();
+            Intent intent = new Intent(MainActivity.this, BrowseMuisc.class);
+            intent.putExtra("tablatureSearch", searchTablature);
+            startActivity(intent);
+        }
+        if (v == mSearchLessonButton) {
+            String searchLesson = mSearchLessons.getText().toString();
+            Intent intent2 = new Intent(MainActivity.this, BrowseLessons.class);
+            intent2.putExtra("lessonSearch", searchLesson);
+            startActivity(intent2);
+        }
     }
 }
