b80e62249f6685ee7d074a552f14b84d19cd95ac
Joey <joeyarnstein@gmail.com>
2016-07-05 14:21:25 -0700

    add template for listview results for search band or song

--- app/src/main/java/com/arnstein/guitarianangel/BrowseMuisc.java
+++ app/src/main/java/com/arnstein/guitarianangel/BrowseMuisc.java
@@ -3,19 +3,37 @@ package com.arnstein.guitarianangel;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class BrowseMuisc extends AppCompatActivity {
     @Bind(R.id.resultTextView) TextView mResultTextView;
+    @Bind(R.id.resultListView) ListView mResultListView;
+    private String[] hardCodedResult = {"Led Zepplin", "Bob Dylan", "Joni Mitchell", "Metallica", "Soundgarden", "Tame Impala", "Kraftwerk", "Green Day", "Passion Pit", "Wood Brothers", "Doesn't Matter"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_muisc);
         ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, hardCodedResult);
+        mResultListView.setAdapter(adapter);
+
+        mResultListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String bandOrSong = ((TextView)view).getText().toString();
+                Toast.makeText(BrowseMuisc.this, bandOrSong, Toast.LENGTH_LONG).show();
+            }
+        });
 
         Intent intent = getIntent();
         String tabSearchName = intent.getStringExtra("tablatureSearch");
